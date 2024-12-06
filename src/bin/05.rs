20e3c39cff84a0369a6eb4c8f7f8e86c5eaaa623
djrobson <djrobson@users.noreply.github.com>
2024-12-06 05:49:02 -0500

    day05 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -44,10 +44,15 @@ fn parse_input1(input: &str) -> (HashMap<u8, Vec<u8>>, HashMap<u8, Vec<u8>>, Vec
     (pre_rules, post_rules, orders)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (pre_rules, post_rules, orders) = parse_input1(input);
-    let mut result: u32 = 0;
-    for order in orders {
+fn split_valid_orders(
+    pre_rules: &HashMap<u8, Vec<u8>>,
+    post_rules: &HashMap<u8, Vec<u8>>,
+    orders: &Vec<Vec<u8>>,
+) -> (Vec<usize>, Vec<usize>) {
+    let mut valid_orders = Vec::new();
+    let mut invalid_orders = Vec::new();
+    for i in 0..orders.len() {
+        let order = orders.get(i).unwrap();
         let mut valid = true;
         // for each value in order
         // if the value is in pre_rules,
@@ -80,16 +85,75 @@ pub fn part_one(input: &str) -> Option<u32> {
             }
         }
         if valid {
-            let middle_value = order[order.len() / 2];
-            //println!("middle value is: {}, {:?}", middle_value, order);
-            result += middle_value as u32;
+            valid_orders.push(i);
+        } else {
+            invalid_orders.push(i);
         }
     }
+    (valid_orders, invalid_orders)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (pre_rules, post_rules, orders) = parse_input1(input);
+    let mut result: u32 = 0;
+    let (valid_orders, _) = split_valid_orders(&pre_rules, &post_rules, &orders);
+    for valid_order in valid_orders {
+        let order = orders.get(valid_order).unwrap();
+        let middle_value = order[order.len() / 2];
+        //println!("middle value is: {}, {:?}", middle_value, order);
+        result += middle_value as u32;
+    }
+
     Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (pre_rules, post_rules, orders) = parse_input1(input);
+    let mut result: u32 = 0;
+    let (_, invalid_orders) = split_valid_orders(&pre_rules, &post_rules, &orders);
+
+    // TODO: consider sorting all numbers 0-99 once then re-sorting each list as a lookup table in linear time
+    for invalid_order in invalid_orders {
+        let order = orders.get(invalid_order).unwrap();
+        // sort order where
+        //  if the value is a key in pre_rules, then this value comes before every value in that pre-rules key
+        //  if the value is a key in post_rules, then this value comes after every value in that pre-rules key
+        let mut order = order.clone();
+        let mut changed = true;
+        while changed {
+            changed = false;
+            for i in 0..order.len() {
+                let value = order[i];
+                if pre_rules.contains_key(&value) {
+                    let my_rules = pre_rules.get(&value).unwrap();
+                    for rule in my_rules {
+                        if let Some(pos) = order.iter().position(|&x| x == *rule) {
+                            if pos > i {
+                                order.swap(i, pos);
+                                changed = true;
+                            }
+                        }
+                    }
+                }
+                if post_rules.contains_key(&value) {
+                    let my_rules = post_rules.get(&value).unwrap();
+                    for rule in my_rules {
+                        if let Some(pos) = order.iter().position(|&x| x == *rule) {
+                            if pos < i {
+                                order.swap(i, pos);
+                                changed = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        let middle_value = order[order.len() / 2];
+        //println!("middle value is: {}, {:?}", middle_value, order);
+        result += middle_value as u32;
+    }
+    Some(result)
 }
 
 #[cfg(test)]
@@ -105,6 +169,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
