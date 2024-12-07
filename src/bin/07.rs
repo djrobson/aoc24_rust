d47c3bcb80a8143d8c07b41f113afa5b69b3e95a
djrobson <djrobson@users.noreply.github.com>
2024-12-07 08:44:37 -0500

    day 07 part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -18,7 +18,7 @@ fn parse_input(input: &str) -> Vec<(usize, Vec<usize>)> {
         .collect()
 }
 
-fn search_operators(total: usize, operands: Vec<usize>) -> bool {
+fn search_operators1(total: usize, operands: Vec<usize>) -> bool {
     if operands.len() == 1 {
         return operands[0] == total;
     }
@@ -32,22 +32,54 @@ fn search_operators(total: usize, operands: Vec<usize>) -> bool {
     let mut mul_operands = Vec::new();
     mul_operands.push(operands[0] * operands[1]);
     mul_operands.extend(&operands[2..]);
-    search_operators(total, add_operands) || search_operators(total, mul_operands)
+    search_operators1(total, add_operands) || search_operators1(total, mul_operands)
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
     let calibrations = parse_input(input);
     let mut total = 0;
     for (cal_total, operands) in calibrations {
-        if search_operators(cal_total, operands) {
+        if search_operators1(cal_total, operands) {
             total += cal_total;
         }
     }
     Some(total)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn search_operators2(total: usize, operands: Vec<usize>) -> bool {
+    if operands.len() == 1 {
+        return operands[0] == total;
+    }
+    if operands[0] > total {
+        return false;
+    }
+    let mut add_operands = Vec::new();
+    add_operands.push(operands[0] + operands[1]);
+    add_operands.extend(&operands[2..]);
+
+    let mut mul_operands = Vec::new();
+    mul_operands.push(operands[0] * operands[1]);
+    mul_operands.extend(&operands[2..]);
+
+    let mut cat_operands: Vec<usize> = Vec::new();
+    let new_first = format!("{}{}", operands[0], operands[1]).parse().unwrap();
+    //dbg!(new_first);
+    cat_operands.push(new_first);
+    cat_operands.extend(&operands[2..]);
+    search_operators2(total, add_operands)
+        || search_operators2(total, mul_operands)
+        || search_operators2(total, cat_operands)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let calibrations = parse_input(input);
+    let mut total = 0;
+    for (cal_total, operands) in calibrations {
+        if search_operators2(cal_total, operands) {
+            total += cal_total;
+        }
+    }
+    Some(total)
 }
 
 #[cfg(test)]
@@ -63,6 +95,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
     }
 }
