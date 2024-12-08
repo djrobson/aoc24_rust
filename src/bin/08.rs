060b2fca640220165716ea4afc5a97459701fb8c
djrobson <djrobson@users.noreply.github.com>
2024-12-08 14:17:42 -0500

    day 08 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,5 +1,6 @@
 advent_of_code::solution!(8);
 
+use std::cmp;
 use std::collections::{HashMap, HashSet};
 
 fn parse_input(input: &str) -> (u8, HashMap<u8, Vec<(u8, u8)>>) {
@@ -78,8 +79,126 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(nodes.len() as u32)
 }
 
+fn get_gcd(x: &i32, y: &i32) -> i32 {
+    let mut greatest_divisor = 1;
+    for divisor in (1..cmp::min(*x, *y)).rev() {
+        /*println!(
+            "{} % {} = {}, {} % {} = {}",
+            x,
+            divisor,
+            x % divisor,
+            y,
+            divisor,
+            y % divisor
+        );*/
+        if x % divisor == 0 && y % divisor == 0 {
+            greatest_divisor = divisor;
+            break;
+        }
+    }
+    //println!("GCD of {} and {} is: {}", x, y, greatest_divisor);
+    greatest_divisor
+}
+
+#[allow(dead_code)]
+fn print_grid(
+    size: usize,
+    antenna_locations: &HashMap<u8, Vec<(u8, u8)>>,
+    nodes: &HashSet<(u8, u8)>,
+) {
+    let mut antenna_collision = 0;
+    let mut grid = vec![vec!['.'; size]; size];
+    for antenna_type in antenna_locations.keys() {
+        let antennas_of_cur_type = antenna_locations.get(antenna_type).unwrap();
+        for antenna in antennas_of_cur_type {
+            grid[antenna.1 as usize][antenna.0 as usize] = *antenna_type as char;
+        }
+    }
+    for node in nodes {
+        if grid[node.1 as usize][node.0 as usize] == '.' {
+            grid[node.1 as usize][node.0 as usize] = '#';
+        } else {
+            antenna_collision += 1;
+        }
+    }
+    for row in grid {
+        println!("{}", row.iter().collect::<String>());
+    }
+    println!("Antenna collisions: {}", antenna_collision);
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (input_size, antenna_locations) = parse_input(input);
+    let mut nodes = HashSet::new();
+
+    for antenna_type in antenna_locations.keys() {
+        let antennas_of_cur_type = antenna_locations.get(antenna_type).unwrap();
+        for antenna in 0..antennas_of_cur_type.len() {
+            let (x1, y1) = antennas_of_cur_type[antenna];
+
+            for other_antenna in antenna + 1..antennas_of_cur_type.len() {
+                let (x2, y2) = antennas_of_cur_type[other_antenna];
+                let x_diff = x1 as i32 - x2 as i32;
+                let y_diff = y1 as i32 - y2 as i32;
+                // reduce x_diff and y_diff if they're divisible by common factor
+                let gcd = get_gcd(&x_diff.abs(), &y_diff.abs());
+                let x_diff = x_diff / gcd;
+                let y_diff = y_diff / gcd;
+
+                // walk down the harmonics until we hit the edge of the grid
+                let mut harmonic = 0;
+                while x1 as i32 - x_diff * harmonic >= 0
+                    && x1 as i32 - x_diff * harmonic <= input_size as i32
+                    && y1 as i32 - y_diff * harmonic >= 0
+                    && y1 as i32 - y_diff * harmonic <= input_size as i32
+                {
+                    /*println!(
+                        "Node: {} ({},{})-({},{}) descending harmonic #{}=({}, {})",
+                        *antenna_type as char,
+                        x1,
+                        y1,
+                        x2,
+                        y2,
+                        harmonic,
+                        x1 as i32 + x_diff * harmonic,
+                        y1 as i32 + y_diff * harmonic
+                    );*/
+                    nodes.insert((
+                        (x1 as i32 - x_diff * harmonic) as u8,
+                        (y1 as i32 - y_diff * harmonic) as u8,
+                    ));
+                    harmonic += 1;
+                }
+
+                // walk up the harmonics until we hit the edge of the grid
+                harmonic = 1;
+                while x1 as i32 + x_diff * harmonic >= 0
+                    && x1 as i32 + x_diff * harmonic <= input_size as i32
+                    && y1 as i32 + y_diff * harmonic >= 0
+                    && y1 as i32 + y_diff * harmonic <= input_size as i32
+                {
+                    /*println!(
+                        "Node: {} ({},{})-({},{}) ascending harmonic #{}=({}, {})",
+                        *antenna_type as char,
+                        x1,
+                        y1,
+                        x2,
+                        y2,
+                        harmonic,
+                        x1 as i32 + x_diff * harmonic,
+                        y1 as i32 + y_diff * harmonic
+                    );*/
+                    nodes.insert((
+                        (x1 as i32 + x_diff * harmonic) as u8,
+                        (y1 as i32 + y_diff * harmonic) as u8,
+                    ));
+                    harmonic += 1;
+                }
+            }
+        }
+    }
+    //print_grid(input_size as usize + 1, &antenna_locations, &nodes);
+    Some(nodes.len() as u32)
 }
 
 #[cfg(test)]
@@ -184,6 +303,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34));
     }
 }
