31349362475b46019b9aa96acea0eea0e42682e2
djrobson <djrobson@users.noreply.github.com>
2024-12-09 17:01:18 -0500

    day 09 part 1 compaction done, checksum WIP

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,29 +1,55 @@
 advent_of_code::solution!(9);
+use std::fmt;
 
-#[derive(Debug, Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 enum Block {
     F(File),
     E(Empty),
 }
 
-#[derive(Debug, Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 struct File {
     size: u32,
     file_index: u32,
 }
 
-#[derive(Debug, Copy,Clone)]
+#[derive(Debug, Copy, Clone)]
 struct Empty {
     size: u32,
 }
-pub fn part_one(input: &str) -> Option<u32> {
 
+impl fmt::Display for Block {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Block::F(file) => {
+                let f_str = (file.file_index % 10).to_string();
+                write!(f, "{}", f_str.repeat(file.size as usize))
+            }
+            Block::E(empty) => write!(f, "{}", ".".repeat(empty.size as usize)),
+        }
+    }
+}
+
+fn get_checksum(blocks: &Vec<Block>) -> u32 {
+    blocks.iter().fold(0, |acc, block| match block {
+        Block::F(f) => {
+            let mut new_sum = 0;
+            for i in f.file_index..f.file_index + f.size {
+                new_sum += i * f.file_index
+            }
+            acc + new_sum
+        }
+        Block::E(_) => acc,
+    })
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
     // parse the input into an alternating sequence of file and empty blocks
     let mut original_blocks: Vec<Block> = Vec::new();
     let mut is_file = true;
     let mut file_index = 0;
     for record in input.bytes() {
-        let size = record as u32;
+        let size = record as u32 - '0' as u32;
         if size > 0 {
             if is_file {
                 original_blocks.push(Block::F(File { size, file_index }));
@@ -35,80 +61,68 @@ pub fn part_one(input: &str) -> Option<u32> {
         is_file = !is_file;
     }
 
-    let mut compact_blocks: Vec<Block> = Vec::new();
+    for block in original_blocks.iter() {
+        print!("{}", block);
+    }
+    println!("");
 
+    let mut compact_blocks: Vec<Block> = Vec::new();
     let mut head_cursor = 0;
     let mut tail_cursor = original_blocks.len() - 1;
     loop {
         match original_blocks[head_cursor] {
             Block::F(head_file) => {
                 compact_blocks.push(Block::F(head_file));
+                head_cursor += 1;
             }
             Block::E(head_empty) => {
-                let mut blocks_to_fill = head_empty.size;
                 // skip all the empty blocks at the end
                 while let Block::E(_) = original_blocks[tail_cursor] {
                     tail_cursor -= 1;
                 }
-                match original_blocks[tail_cursor] {
-                    Block::F(tail_file) => {
-                        // if the empty block at head_cursor is the same size as the file at tail_cursor
-                        //  the move the while thing into the compact_blocks
-                        // else if the empty block at head_cursor is larger than the file at tail_cursor
-                        //  then move the file at tail_cursor into the compact_blocks and decrement blocks_to_fill
-                        //  add more file blocks to the compact_blocks until blocks_to_fill is 0
-                        // else if the empty block at head_cursor is smaller than the file at tail_cursor
-                        //  then move some of the file at tail_cursor into the compact_blocks and decrement tail file size by the amount moved
-                        if tail_file.size == head_empty.size {
-                            compact_blocks.push(Block::F(tail_file));
-                            tail_cursor -= 1;
-                        } else if tail_file.size < blocks_to_fill {
-                            compact_blocks.push(Block::F(tail_file));
-                            blocks_to_fill -= tail_file.size;
-                            while blocks_to_fill > 0 {
-                                match original_blocks[tail_cursor] {
-                                    Block::F(f) => {
-                                        compact_blocks.push(Block::F(f));
-                                        blocks_to_fill -= 1;
-                                    }
-                                    Block::E(_) => {
-                                        panic!("we just skipped all these");
-                                    }
-                                }
-                            }
-                        } else {
-                            let mut smaller_tail_file = tail_file.clone();
-                            let size_left = tail_file.size - blocks_to_fill;
-                            smaller_tail_file.size = blocks_to_fill;
-                            blocks_to_fill = 0;
-                            compact_blocks.push(Block::F(smaller_tail_file));
-                            //replace the tail file with the smaller tail file
-                            // TODO loop around and stick the rest of the file someplace?
-                            original_blocks[tail_cursor] = Block::F(File { size: size_left, file_index: tail_file.file_index });
-                        }
+                if let Block::F(tail_file) = original_blocks[tail_cursor] {
+                    if tail_file.size == head_empty.size {
+                        compact_blocks.push(Block::F(tail_file));
+                        tail_cursor -= 1;
+                        head_cursor += 1;
+                    } else if tail_file.size < head_empty.size {
+                        compact_blocks.push(Block::F(tail_file));
+
+                        //replace the empty block at the head with a smaller one
+                        original_blocks[head_cursor] = Block::E(Empty {
+                            size: head_empty.size - tail_file.size,
+                        });
+                        tail_cursor -= 1;
+                    } else {
+                        let mut smaller_tail_file = tail_file.clone();
+                        let size_left = tail_file.size - head_empty.size;
+                        smaller_tail_file.size = head_empty.size;
+                        compact_blocks.push(Block::F(File {
+                            size: head_empty.size,
+                            file_index: tail_file.file_index,
+                        })); // replace the file block at the tail with a smaller one
+                        original_blocks[tail_cursor] = Block::F(File {
+                            size: size_left,
+                            file_index: tail_file.file_index,
+                        });
+                        head_cursor += 1;
                     }
-                    Block::E(_) => {
-                        panic!("we just skipped all these");
-                    }
+                } else {
+                    panic!("unexpected  block type");
                 }
             }
         }
-        head_cursor += 1;
         if head_cursor >= tail_cursor {
             break;
         }
     }
-    let checksum = compact_blocks.iter().fold(0, |acc, block| {
-        match block {
-            Block::F(f) => {
-                let mut new_sum = 0;
-                for i in f.file_index..f.file_index + f.size {
-                    new_sum += i * f.file_index
-                }
-                acc + new_sum},
-            Block::E(_) => acc,
-        }
-    });
+
+    for block in compact_blocks.iter() {
+        print!("{}", block);
+    }
+    println!("");
+
+    let checksum = get_checksum(&compact_blocks);
     Some(checksum)
 }
 
