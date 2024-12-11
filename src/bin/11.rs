78558b3a572b9fd528a9e4c376e68168b8e54fff
djrobson <djrobson@users.noreply.github.com>
2024-12-11 11:43:00 -0500

    day 11 part 2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -38,8 +38,96 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(output_len as u32)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn build_list_from_seed(seed: usize, depth: usize) -> Vec<usize> {
+    let mut this_input = vec![seed];
+    for _ in 0..depth {
+        let mut new_input = Vec::new();
+        for num in this_input.iter() {
+            if *num == 0 {
+                new_input.push(1);
+            } else if get_num_digits(num) % 2 == 0 {
+                let printed_num = num.to_string();
+                let half = printed_num.len() / 2;
+
+                new_input.push(printed_num[..half].parse().unwrap());
+                new_input.push(printed_num[half..].parse().unwrap());
+            } else {
+                new_input.push(num * 2024);
+            }
+        }
+        this_input = new_input;
+    }
+    this_input
+}
+
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+struct Node {
+    value: usize,
+    depth: usize,
+}
+
+impl Node {
+    fn new(value: usize, depth: usize) -> Self {
+        Self { value, depth }
+    }
+}
+
+fn count_nodes_at_depth(
+    node: &Node,
+    count_cache: &mut std::collections::HashMap<Node, usize>,
+) -> usize {
+    if node.depth == 0 {
+        return 1;
+    }
+
+    if let Some(count) = count_cache.get(node) {
+        return *count;
+    }
+
+    let mut count = 0;
+    if node.value == 0 {
+        count += count_nodes_at_depth(&Node::new(1, node.depth - 1), count_cache);
+    } else if get_num_digits(&node.value) % 2 == 0 {
+        let printed_num = node.value.to_string();
+        let half = printed_num.len() / 2;
+
+        count += count_nodes_at_depth(
+            &Node::new(printed_num[..half].parse().unwrap(), node.depth - 1),
+            count_cache,
+        );
+        count += count_nodes_at_depth(
+            &Node::new(printed_num[half..].parse().unwrap(), node.depth - 1),
+            count_cache,
+        );
+    } else {
+        count += count_nodes_at_depth(&Node::new(node.value * 2024, node.depth - 1), count_cache);
+    }
+
+    count_cache.insert(*node, count);
+    count
+}
+
+fn part_two_with_depth(input: &str, depth: usize) -> Option<usize> {
+    let this_input: Vec<Node> = input
+        .split_ascii_whitespace()
+        .map(|num| Node {
+            value: num.parse().expect("Invalid number"),
+            depth,
+        })
+        .collect::<Vec<_>>();
+
+    let mut output_len: usize = 0;
+    let mut count_cache = std::collections::HashMap::new();
+    for node in this_input.iter() {
+        output_len += count_nodes_at_depth(node, &mut count_cache);
+    }
+
+    Some(output_len)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let max_depth = 75;
+    return part_two_with_depth(input, max_depth);
 }
 
 #[cfg(test)]
@@ -53,8 +141,8 @@ mod tests {
     }
 
     #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn test_part_two_with_depth() {
+        let result = part_two_with_depth(&advent_of_code::template::read_file("examples", DAY), 25);
+        assert_eq!(result, Some(55312));
     }
 }
