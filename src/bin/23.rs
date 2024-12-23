445682a1bb86d3d766456bf7fa6726ba904ee69b
djrobson <djrobson@users.noreply.github.com>
2024-12-23 14:59:54 -0500

    day 23 part 2

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -13,8 +13,6 @@ fn parse_input_graph_map(input: &str) -> UnGraphMap<&str, ()> {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    //let input_graph = Frozen::new(&mut parse_input_ungraph(input));
-    //let triple = Frozen::new(&mut get_triple_ungraph());
     let input_graph = &parse_input_graph_map(input);
 
     let count = input_graph
@@ -40,8 +38,52 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(count as u32)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<String> {
+    let input_graph = &parse_input_graph_map(input);
+
+    let mut clusters_found;
+    let mut biggest_cluster: Option<Vec<&str>> = None;
+
+    for cluster_size in [3, 12].iter() {
+        let cluster = input_graph
+            .nodes()
+            .flat_map(|node| {
+                input_graph
+                    .neighbors(node) // for each node we connect to
+                    .combinations(*cluster_size as usize)
+                    .filter_map(
+                        // make pairs of all of them
+                        move |neighbor_subset| {
+                            if neighbor_subset
+                                .iter()
+                                .tuple_combinations()
+                                .all(move |(a, b)| input_graph.contains_edge(a, b))
+                            {
+                                // sort the triples cannonically
+                                let mut nodes = vec![node]
+                                    .into_iter()
+                                    .chain(neighbor_subset.into_iter())
+                                    .collect::<Vec<_>>();
+                                nodes.sort();
+                                Some(nodes)
+                            } else {
+                                None
+                            }
+                        },
+                    )
+            })
+            .unique() // get rid of duplicates
+            .collect::<Vec<_>>();
+        clusters_found = cluster.len();
+        if clusters_found == 1 {
+            biggest_cluster = Some(cluster[0].clone());
+        }
+    }
+    if let Some(biggest_cluster) = biggest_cluster {
+        Some(biggest_cluster.join(",").to_string())
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -52,5 +94,10 @@ mod tests {
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(7));
+    }
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(["co", "de", "ka", "ta"].join(",")));
     }
 }
