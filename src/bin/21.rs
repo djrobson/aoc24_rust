c22952ec15908d2352bdcda1cb1dcf3145a04867
djrobson <drobson@gmail.com>
2024-12-22 21:55:50 -0500

    day21 part 2 lots of work

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -77,7 +77,7 @@ fn traverse_dir_pad(start_pos: &DirPad, end_pos: &DirPad) -> (DirPad, String) {
         (DirPad::Right, DirPad::Right) => String::new(),
         (DirPad::Right, DirPad::A) => "^".to_string(),
         (DirPad::A, DirPad::Up) => "<".to_string(),
-        (DirPad::A, DirPad::Down) => "v<".to_string(),
+        (DirPad::A, DirPad::Down) => "<v".to_string(),
         (DirPad::A, DirPad::Left) => "v<<".to_string(),
         (DirPad::A, DirPad::Right) => "v".to_string(),
         (DirPad::A, DirPad::A) => String::new(),
@@ -102,8 +102,6 @@ fn traverse_dir_pad_for_all(inputs: &str) -> String {
             },
         );
         commands.push_str(&num_commands);
-        //let (new_dir_pad_pos, num_commands) = traverse_dir_pad(&dir_pad_pos, &DirPad::A);
-        //commands.push_str(&num_commands);
         commands.push('A');
         dir_pad_pos = new_dir_pad_pos;
     }
@@ -133,17 +131,19 @@ fn traverse_num_pad(start_pos: &NumPad, end_pos: &NumPad) -> (NumPad, String) {
         (NumPad::One, NumPad::Nine) => ">>^^".to_string(),
         (NumPad::One, NumPad::Zero) => ">v".to_string(),
         (NumPad::One, NumPad::A) => ">>v".to_string(),
+
         (NumPad::Two, NumPad::One) => "<".to_string(),
         (NumPad::Two, NumPad::Two) => String::new(),
         (NumPad::Two, NumPad::Three) => ">".to_string(),
         (NumPad::Two, NumPad::Four) => "<^".to_string(),
         (NumPad::Two, NumPad::Five) => "^".to_string(),
-        (NumPad::Two, NumPad::Six) => ">^".to_string(),
+        (NumPad::Two, NumPad::Six) => "^>".to_string(),
         (NumPad::Two, NumPad::Seven) => "<^^".to_string(),
         (NumPad::Two, NumPad::Eight) => "^^".to_string(),
         (NumPad::Two, NumPad::Nine) => ">^^".to_string(),
         (NumPad::Two, NumPad::Zero) => "v".to_string(),
-        (NumPad::Two, NumPad::A) => ">v".to_string(),
+        (NumPad::Two, NumPad::A) => "v>".to_string(),
+
         (NumPad::Three, NumPad::One) => "<<".to_string(),
         (NumPad::Three, NumPad::Two) => "<".to_string(),
         (NumPad::Three, NumPad::Three) => String::new(),
@@ -153,8 +153,9 @@ fn traverse_num_pad(start_pos: &NumPad, end_pos: &NumPad) -> (NumPad, String) {
         (NumPad::Three, NumPad::Seven) => "<<^^".to_string(),
         (NumPad::Three, NumPad::Eight) => "<^^".to_string(),
         (NumPad::Three, NumPad::Nine) => "^^".to_string(),
-        (NumPad::Three, NumPad::Zero) => "v<".to_string(),
+        (NumPad::Three, NumPad::Zero) => "<v".to_string(),
         (NumPad::Three, NumPad::A) => "v".to_string(),
+
         (NumPad::Four, NumPad::One) => "v".to_string(),
         (NumPad::Four, NumPad::Two) => "v>".to_string(),
         (NumPad::Four, NumPad::Three) => "v>>".to_string(),
@@ -166,7 +167,8 @@ fn traverse_num_pad(start_pos: &NumPad, end_pos: &NumPad) -> (NumPad, String) {
         (NumPad::Four, NumPad::Nine) => ">>^".to_string(),
         (NumPad::Four, NumPad::Zero) => ">vv".to_string(),
         (NumPad::Four, NumPad::A) => ">>vv".to_string(),
-        (NumPad::Five, NumPad::One) => "v<".to_string(),
+
+        (NumPad::Five, NumPad::One) => "<v".to_string(),
         (NumPad::Five, NumPad::Two) => "v".to_string(),
         (NumPad::Five, NumPad::Three) => "v>".to_string(),
         (NumPad::Five, NumPad::Four) => "<".to_string(),
@@ -174,11 +176,12 @@ fn traverse_num_pad(start_pos: &NumPad, end_pos: &NumPad) -> (NumPad, String) {
         (NumPad::Five, NumPad::Six) => ">".to_string(),
         (NumPad::Five, NumPad::Seven) => "<^".to_string(),
         (NumPad::Five, NumPad::Eight) => "^".to_string(),
-        (NumPad::Five, NumPad::Nine) => ">^".to_string(),
+        (NumPad::Five, NumPad::Nine) => "^>".to_string(),
         (NumPad::Five, NumPad::Zero) => "vv".to_string(),
-        (NumPad::Five, NumPad::A) => ">vv".to_string(),
-        (NumPad::Six, NumPad::One) => "v<<".to_string(),
-        (NumPad::Six, NumPad::Two) => "v<".to_string(),
+        (NumPad::Five, NumPad::A) => "vv>".to_string(),
+
+        (NumPad::Six, NumPad::One) => "<<v".to_string(),
+        (NumPad::Six, NumPad::Two) => "<v".to_string(),
         (NumPad::Six, NumPad::Three) => "v".to_string(),
         (NumPad::Six, NumPad::Four) => "<<".to_string(),
         (NumPad::Six, NumPad::Five) => "<".to_string(),
@@ -186,10 +189,11 @@ fn traverse_num_pad(start_pos: &NumPad, end_pos: &NumPad) -> (NumPad, String) {
         (NumPad::Six, NumPad::Seven) => "<<^".to_string(),
         (NumPad::Six, NumPad::Eight) => "<^".to_string(),
         (NumPad::Six, NumPad::Nine) => "^".to_string(),
-        (NumPad::Six, NumPad::Zero) => "vv<".to_string(),
+        (NumPad::Six, NumPad::Zero) => "<vv".to_string(),
         (NumPad::Six, NumPad::A) => "vv".to_string(),
+
         (NumPad::Seven, NumPad::One) => "vv".to_string(),
-        (NumPad::Seven, NumPad::Two) => ">vv".to_string(),
+        (NumPad::Seven, NumPad::Two) => "vv>".to_string(),
         (NumPad::Seven, NumPad::Three) => "vv>>".to_string(),
         (NumPad::Seven, NumPad::Four) => "v".to_string(),
         (NumPad::Seven, NumPad::Five) => "v>".to_string(),
@@ -199,10 +203,11 @@ fn traverse_num_pad(start_pos: &NumPad, end_pos: &NumPad) -> (NumPad, String) {
         (NumPad::Seven, NumPad::Nine) => ">>".to_string(),
         (NumPad::Seven, NumPad::Zero) => ">vvv".to_string(),
         (NumPad::Seven, NumPad::A) => ">>vvv".to_string(),
-        (NumPad::Eight, NumPad::One) => "vv<".to_string(),
+
+        (NumPad::Eight, NumPad::One) => "<vv".to_string(),
         (NumPad::Eight, NumPad::Two) => "vv".to_string(),
         (NumPad::Eight, NumPad::Three) => "vv>".to_string(),
-        (NumPad::Eight, NumPad::Four) => "v<".to_string(),
+        (NumPad::Eight, NumPad::Four) => "<v".to_string(),
         (NumPad::Eight, NumPad::Five) => "v".to_string(),
         (NumPad::Eight, NumPad::Six) => "v>".to_string(),
         (NumPad::Eight, NumPad::Seven) => "<".to_string(),
@@ -210,17 +215,19 @@ fn traverse_num_pad(start_pos: &NumPad, end_pos: &NumPad) -> (NumPad, String) {
         (NumPad::Eight, NumPad::Nine) => ">".to_string(),
         (NumPad::Eight, NumPad::Zero) => "vvv".to_string(),
         (NumPad::Eight, NumPad::A) => "vvv>".to_string(),
-        (NumPad::Nine, NumPad::One) => "vv<<".to_string(),
-        (NumPad::Nine, NumPad::Two) => "vv<".to_string(),
+
+        (NumPad::Nine, NumPad::One) => "<<vv".to_string(),
+        (NumPad::Nine, NumPad::Two) => "<vv".to_string(),
         (NumPad::Nine, NumPad::Three) => "vv".to_string(),
-        (NumPad::Nine, NumPad::Four) => "v<<".to_string(),
-        (NumPad::Nine, NumPad::Five) => "v<".to_string(),
+        (NumPad::Nine, NumPad::Four) => "<<v".to_string(),
+        (NumPad::Nine, NumPad::Five) => "<v".to_string(),
         (NumPad::Nine, NumPad::Six) => "v".to_string(),
         (NumPad::Nine, NumPad::Seven) => "<<".to_string(),
         (NumPad::Nine, NumPad::Eight) => "<".to_string(),
         (NumPad::Nine, NumPad::Nine) => String::new(),
-        (NumPad::Nine, NumPad::Zero) => "vvv<".to_string(),
+        (NumPad::Nine, NumPad::Zero) => "<vvv".to_string(),
         (NumPad::Nine, NumPad::A) => "vvv".to_string(),
+
         (NumPad::Zero, NumPad::One) => "^<".to_string(),
         (NumPad::Zero, NumPad::Two) => "^".to_string(),
         (NumPad::Zero, NumPad::Three) => ">^".to_string(),
@@ -232,6 +239,7 @@ fn traverse_num_pad(start_pos: &NumPad, end_pos: &NumPad) -> (NumPad, String) {
         (NumPad::Zero, NumPad::Nine) => ">^^^".to_string(),
         (NumPad::Zero, NumPad::Zero) => String::new(),
         (NumPad::Zero, NumPad::A) => ">".to_string(),
+
         (NumPad::A, NumPad::One) => "^<<".to_string(),
         (NumPad::A, NumPad::Two) => "<^".to_string(),
         (NumPad::A, NumPad::Three) => "^".to_string(),
@@ -276,14 +284,13 @@ pub fn part_one(input: &str) -> Option<usize> {
     let inputs = parse_inputs(input);
     let mut complexity = 0;
     for input in inputs {
-        println!("input: {:?}", input);
-        //let mut me_commands = String::new();
         let mut robot1_commands = String::new();
         let mut robot2_commands = String::new();
-        //let mut all_me_commands = String::new();
+
         let mut all_numpad_commands = String::new();
         let mut all_robot1_commands = String::new();
         let mut all_robot2_commands = String::new();
+
         // starting at the bottom right corner
         let mut num_pad_pos = NumPad::A;
         for num_pad_button in &input {
@@ -302,17 +309,12 @@ pub fn part_one(input: &str) -> Option<usize> {
             robot2_commands.push_str(&dir2_commands);
             all_robot2_commands.push_str(&dir2_commands);
             //println!("robot2: {}", dir2_commands);
-
-            //let (_dir3_pos, dir3_commands) = traverse_dir_pad_for_all(&DirPad::A, &dir2_commands);
-            //me_commands.push_str(&dir3_commands);
-            //println!("me:     {}", dir3_commands);
         }
+
+        //println!("all robot 2 commands: {}", all_robot2_commands);
+        //println!("all robot 1 commands: {}", all_robot1_commands);
+        //println!("all numpad commands: {}", all_numpad_commands);
         complexity += all_robot2_commands.len() * get_num_from_input(&input);
-        //all_me_commands.push_str(&me_commands);
-        //println!("all me commands: {}", all_me_commands);
-        println!("all robot 2 commands: {}", all_robot2_commands);
-        println!("all robot 1 commands: {}", all_robot1_commands);
-        println!("all numpad commands: {}", all_numpad_commands);
     }
     Some(complexity)
 }
@@ -325,18 +327,21 @@ fn robot_presses(
     let input_count;
 
     if let Some(count) = cache.get(&(presses.to_owned(), robot_count)) {
+        // we have already computed the count for this input
         input_count = *count;
     } else {
+        // break the input input parts and add the cost of all parts
         let dir_pushes = traverse_dir_pad_for_all(presses);
         if robot_count == 0 {
             input_count = dir_pushes.len();
         } else {
-            input_count = dir_pushes.chars().fold(0, |sum, byte| {
-                // computer count for each byte at robot_count - 1
-                sum + robot_presses(&byte.to_string(), robot_count - 1, cache)
-            });
-            cache.insert((presses.to_owned(), robot_count), input_count);
+            // split input by A and recurse on each part
+            input_count = dir_pushes.split_inclusive('A').map(|part| {
+                // compute count for each part at the next level
+                robot_presses(&part, robot_count - 1, cache)
+            }).sum();
         }
+        cache.insert((presses.to_owned(), robot_count), input_count);
     }
     input_count
 }
@@ -349,15 +354,14 @@ pub fn part_two_ex(input: &str, robot_count: usize) -> Option<usize> {
     let inputs = parse_inputs(input);
     let mut total_complexity = 0;
     let mut push_cache: HashMap<(String, usize), usize> = HashMap::new();
+    let mut num_pad_pos = NumPad::A;
     for input in inputs {
-        println!("input: {:?}", input);
-        // starting at the bottom right corner
-        let mut num_pad_pos = NumPad::A;
         for num_pad_button in &input {
-            // traverse the num pad by moving the cursor to the desired number
+            // for each number on the number, move the robot and remember where it is
             let (new_num_pad_pos, num_commands) = traverse_num_pad(&num_pad_pos, num_pad_button);
-            num_pad_pos = new_num_pad_pos;
-            let input_count = robot_presses(&num_commands, robot_count, &mut push_cache);
+            num_pad_pos = new_num_pad_pos; // update the position of the numpad robot
+
+            let input_count = robot_presses(&num_commands, robot_count-1, &mut push_cache);
 
             total_complexity += input_count * get_num_from_input(&input);
         }
@@ -368,6 +372,61 @@ pub fn part_two_ex(input: &str, robot_count: usize) -> Option<usize> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(175396398527088));
+    }
+
+    #[test]
+    fn test_part_two_ex() {
+        let result = part_two_ex(&advent_of_code::template::read_file("examples", DAY), 2);
+        assert_eq!(result, Some(126384));
+    }
+
+    #[test]
+    fn test_part_two_029() {
+        let result = part_two_ex("029A", 2);
+        assert_eq!(
+            result,
+            Some("<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A".len() * 29)
+        );
+    }
+    #[test]
+    fn test_part_two_980() {
+        let result = part_two_ex("980A", 2);
+        assert_eq!(
+            result,
+            Some("<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A".len() * 980)
+        );
+    }
+    #[test]
+    fn test_part_two_179() {
+        let result = part_two_ex("179A", 2);
+        assert_eq!(
+            result,
+            Some(
+                "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A".len() * 179
+            )
+        );
+    }
+    #[test]
+    fn test_part_two_456() {
+        let result = part_two_ex("456A", 2);
+        assert_eq!(
+            result,
+            Some("<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A".len() * 456)
+        );
+    }
+    #[test]
+    fn test_part_two_379() {
+        let result = part_two_ex("379A", 2);
+        assert_eq!(
+            result,
+            Some("<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A".len() * 379)
+        );
+    }
 
     #[test]
     fn test_part_one() {
@@ -416,67 +475,5 @@ mod tests {
             result,
             Some("<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A".len() * 379)
         );
-    }
-
-    #[test]
-    #[ignore]
-    fn test_part_two_ex() {
-        let result = part_two_ex(&advent_of_code::template::read_file("examples", DAY), 2);
-        assert_eq!(result, Some(126384));
-    }
-
-    #[test]
-    #[ignore]
-    fn test_part_two_029() {
-        let result = part_two_ex("029A", 2);
-        assert_eq!(
-            result,
-            Some("<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A".len() * 29)
-        );
-    }
-    #[test]
-    #[ignore]
-    fn test_part_two_980() {
-        let result = part_two_ex("980A", 2);
-        assert_eq!(
-            result,
-            Some("<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A".len() * 980)
-        );
-    }
-    #[test]
-    #[ignore]
-    fn test_part_two_179() {
-        let result = part_two_ex("179A", 2);
-        assert_eq!(
-            result,
-            Some(
-                "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A".len() * 179
-            )
-        );
-    }
-    #[test]
-    #[ignore]
-    fn test_part_two_456() {
-        let result = part_two_ex("456A", 2);
-        assert_eq!(
-            result,
-            Some("<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A".len() * 456)
-        );
-    }
-    #[test]
-    #[ignore]
-    fn test_part_two_379() {
-        let result = part_two_ex("379A", 2);
-        assert_eq!(
-            result,
-            Some("<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A".len() * 379)
-        );
-    }
-
-    #[test]
-    #[ignore]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
     }
 }
